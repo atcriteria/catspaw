acf0242f13f1f5a803a98994c435c6c3dbec4943
dude lao <brycelipscomb@gmail.com>
2021-07-31 22:47:35 -0400

    messing around with API calls. Tried grabbing dominant color from images but was unable to find a method to extract a primary color from an image received from the API call

---
+++ src/components/CatFinder.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+export default function CatFinger(){
+    const [imgURL, setImgURL] = useState("")
+    const [fact, setFact] = useState("")
+    const baseURL = 'https://cataas.com'
+
+    const configureImage = (url) =>{
+        if (typeof(url) === 'string'){
+            setImgURL(baseURL+url)
+        }
+    }
+
+    console.log(`new imgurl = ${imgURL}`)
+    useEffect(() => {
+        axios.get('https://cataas.com/cat?json=true')
+        .then(res => {
+            console.log(res.data.url)
+            configureImage(res.data.url)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        axios.get('https://catfact.ninja/fact')
+        .then(res => {
+            setFact(res.data.fact)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+    
+    return(
+        <div>
+            Cat finder here plzz
+            <p>image url:{imgURL}</p>
+            <img id="cat-photo" src={imgURL} alt="Cat photograph from the Cats As A Service API" />
+            <p>Random Cat Fact: {fact}</p>
+        </div>
+    )
+}
